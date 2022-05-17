7c8dfe93012b904161bd8f2dae93c0fffd85407b
Díaz Urbaneja Víctor Diego Alejandro <sodomon2@gmail.com>
2022-05-17 17:49:27 -0400

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+/**!
+ * @package   
+ * @filename  main.js
+ * @version   1.0
+ * @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon@openmailbox.org>
+ * @date      
+ */
+
+console.log("Bienvenido:)\nesto es una prueba jsjsjs")
